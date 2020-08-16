56c80735c74025415f0b922d9c335786111152db
Olena Kaliuzhna <om.kaliuzhna.dz@gmail.com>
2020-08-16 16:15:46 +0300

    adds check for task-06

--- js/task-06.js
+++ js/task-06.js
@@ -3,6 +3,11 @@ let total = 0;
 
 do {
   input = prompt('Введите число');
-  total += Number(input);
+
+  if (isNaN(input)) {
+    alert('Было введено не число, попробуйте еще раз');
+  } else {
+    total += Number(input);
+  }
 } while (input !== null);
 alert(`Общая сумма чисел равна ${total}`);
